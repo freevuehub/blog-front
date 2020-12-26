7b02f8f2201154eea146302e579038f114d1515c
doo9392 <hackish@hanmail.net>
2020-12-26 22:20:49 +0900

    [improved]: theme 구성 확인

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import { ApolloProvider } from '@apollo/client'
-import { client } from '../lib'
+import { client, GlobalStyle } from '../lib'
 import { Layout } from '../components'
+import { Global, ThemeProvider } from '@emotion/react'
 
 interface IProps {
   Component: any
   pageProps: any
 }
 
+const mode = 'light'
+const colorSet = {
+  light: {
+    primary: '#6bb3b8',
+    background: {
+      app: '#fff',
+      content: '#fff',
+    },
+    text: '#37352f',
+  },
+  dark: {
+    primary: '#6bb3b8',
+    background: {
+      app: '#121212',
+      content: '#1e1e1e',
+    },
+    text: '#fff',
+  },
+}
+const theme = colorSet[mode]
 const App: React.FC<IProps> = (props) => {
   return (
     <ApolloProvider client={client}>
-      <Layout>
-        <props.Component {...props.pageProps} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyle} />
+        <Layout>
+          <props.Component {...props.pageProps} />
+        </Layout>
+      </ThemeProvider>
     </ApolloProvider>
   )
 }
