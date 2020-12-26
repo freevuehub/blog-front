c79af9a75844257f6ba13c8f2f86750343a1bbff
doo9392 <hackish@hanmail.net>
2020-12-26 22:18:37 +0900

    [new]: header 구성 추가

--- components/HeaderBar.tsx
+++ components/HeaderBar.tsx
@@ -1,21 +1,43 @@
 import React, { useState, useEffect } from 'react'
-import { client, gql } from '../lib'
+import styled from '@emotion/styled'
+import { client, ITheme } from '../lib'
+import { categories } from '../gql'
+import Link from 'next/link'
+import Menu from './Menu'
 
+const HeaderStyled = styled.header`
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  height: 60px;
+  box-shadow: 0 3px 4px 0 rgba(0,0,0,.03);
+  z-index: 50;
+  display: flex;
+  padding: 0 20px;
+  align-items: center;
+  ${(props: ITheme) => ({
+    backgroundColor: props.theme?.background.content,
+    color: props.theme?.text,
+  })}
+  h1 {
+    font-size: 20px;
+    font-weight: 700;
+    a {
+      ${(props: ITheme) => ({ color: props.theme?.text })}
+      &:hover {
+        transition: color 0.3s;
+        ${(props: ITheme) => ({ color: props.theme?.primary })}
+      }
+    }
+  }
+`
 const HeaderBar: React.FC = () => {
   const [list, setList] = useState([])
 
   const getCategories = async () => {
     try {
-      const { data } = await client.query({
-        query: gql`
-          {
-            categories {
-              id
-              name
-            }
-          }
-        `
-      })
+      const { data } = await client.query(categories())
 
       setList(data.categories)
     } catch {
@@ -27,20 +49,12 @@ const HeaderBar: React.FC = () => {
   })
 
   return (
-    <header>
-      <h1>FreeVue Blog</h1>
-      <nav>
-        <ul>
-          {
-            list.map((item: any) => {
-              return (
-                <li key={item.id}>{item.name}</li>
-              )
-            })
-          }
-        </ul>
-      </nav>
-    </header>
+    <HeaderStyled>
+      <h1 className="ibmplexsans">
+        <Link href="/">FreeVue Blog</Link>
+      </h1>
+      <Menu list={list} />
+    </HeaderStyled>
   )
 }
 
---
+++ components/Menu.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import Link from 'next/link'
+import { ICategory } from '../types'
+import { ITheme } from '../lib'
+
+interface IProps {
+  list: ICategory[]
+}
+
+const MenuStyled = styled.header`
+  margin-left: auto;
+  ul {
+    display: flex;
+    li {
+      margin-left: 24px;
+      a {
+        text-transform: capitalize;
+        ${(props: ITheme) => ({ color: props.theme?.text })}
+        &:hover {
+          transition: color 0.3s;
+          ${(props: ITheme) => ({ color: props.theme?.primary })}
+        }
+      }
+    }
+  }
+`
+
+const listMap = (item: ICategory) => {
+  return (
+    <li key={item.id}>
+      <Link href={`/${item.name}`}>{item.name}</Link>
+    </li>
+  )
+}
+const Menu: React.FC<IProps> = (props) => {
+  return (
+    <MenuStyled>
+      <ul className="gmarketsans">
+        {props.list.map(listMap)}
+      </ul>
+    </MenuStyled>
+  )
+}
+
+export default Menu
