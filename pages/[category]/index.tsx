dc61a5943f4d4f335702e78be3abed856cc4ef08
doo9392 <hackish@hanmail.net>
2020-12-28 23:09:05 +0900

    [improved]: 블로게시글 로드하는 부분 추가

--- pages/[category]/index.tsx
+++ pages/[category]/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import styled from '@emotion/styled'
-import { CategoryTitle } from '../../components'
+import { client } from '../../lib'
+import { posts } from '../../gql'
+import { CategoryTitle, PostList } from '../../components'
+
+interface IPostListItem {
+  id: string
+  category: string
+  image: string
+  title: string
+  description: string
+  createDate: string
+}
 
 const CategoryPageStyled = styled.article`
   margin: 100px auto 0;
@@ -10,10 +21,25 @@ const CategoryPageStyled = styled.article`
 
 const CategoryPage: React.FC = () => {
   const router = useRouter()
+  const [list, setList] = useState<IPostListItem[]>([])
+  const getPosts = async () => {
+    try {
+      const { data } = await client.query(posts())
+
+      setList(data.list)
+    } catch {
+
+    }
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [router.query.category])
 
   return (
     <CategoryPageStyled>
       <CategoryTitle>{router.query.category}</CategoryTitle>
+      <PostList list={list} />
     </CategoryPageStyled>
   )
 }
