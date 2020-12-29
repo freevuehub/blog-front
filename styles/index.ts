1ed28bdb935801c6c858a52c8970096c3b91dfc7
doo9392 <hackish@hanmail.net>
2020-12-29 19:16:34 +0900

    [improved]: 전역 style 적용

--- styles/index.ts
+++ styles/index.ts
@@ -1,2 +1,4 @@
 export { default as FontStyle } from './Font'
 export { default as ResetStyle } from './Reset'
+export { default as MarkdownStyle } from './Markdown'
+export { default as CodeStyle } from './Code'
