291564be58c597e795f245c6052870efade6e8af
doo9392 <hackish@hanmail.net>
2021-01-03 18:45:19 +0900

    [UX/UI]: 헤더 스타일 수정

--- components/HeaderBar.tsx
+++ components/HeaderBar.tsx
@@ -20,37 +20,41 @@ const HeaderStyled = styled.header`
   left: 0;
   top: 0;
   right: 0;
-  height: 60px;
   z-index: 50;
-  display: flex;
   padding: 0 20px;
-  align-items: center;
   ${(props: ITheme) => ({
     backgroundColor: props.theme?.background.content,
     color: props.theme?.text,
     boxShadow: props.theme?.shadow.new,
   })}
-  h1 {
-    font-size: 20px;
-    font-weight: 700;
-    a {
-      ${(props: ITheme) => ({ color: props.theme?.text })}
+  .inner {
+    height: 60px;
+    margin: 0 auto;
+    max-width: 960px;
+    display: flex;
+    align-items: center;
+    h1 {
+      font-size: 20px;
+      font-weight: 700;
+      a {
+        ${(props: ITheme) => ({ color: props.theme?.text })}
 
-      @media (min-width: 601px) {
-        &:hover {
-          transition: color 0.3s;
-          ${(props: ITheme) => ({ color: props.theme?.primary })}
+        @media (min-width: 601px) {
+          &:hover {
+            transition: color 0.3s;
+            ${(props: ITheme) => ({ color: props.theme?.primary })}
+          }
         }
       }
     }
-  }
-  .menu-btn {
-    margin-right: auto;
-    font-size: 20px;
-    ${(props: ITheme) => ({ color: props.theme?.text })}
+    .menu-btn {
+      margin-right: auto;
+      font-size: 20px;
+      ${(props: ITheme) => ({ color: props.theme?.text })}
 
-    @media (min-width: 841px) {
-      display: none;
+      @media (min-width: 841px) {
+        display: none;
+      }
     }
   }
 `
@@ -81,24 +85,26 @@ const HeaderBar: React.FC<IProps> = (props) => {
 
   return (
     <HeaderStyled>
-      <button className="menu-btn" onClick={onMenuClick}>
-        <Icon icon={faBars} />
-      </button>
-      <h1 className="ibmplexsans">
-        <Link href="/">FreeVue Blog</Link>
-      </h1>
-      {/* <iframe
-        src="https://ghbtns.com/github-btn.html?user=freevuehub&repo=freevuehub.github.io&type=star&count=true&size=large"
-        scrolling="0"
-        width="120px"
-        height="30px"
-      /> */}
-      <Menu
-        list={list}
-        className={props.className}
-        onBgClick={onMenuBgClick}
-      />
-      <SearchInput />
+      <div className="inner">
+        <button className="menu-btn" onClick={onMenuClick}>
+          <Icon icon={faBars} />
+        </button>
+        <h1 className="ibmplexsans">
+          <Link href="/">FreeVue Blog</Link>
+        </h1>
+        {/* <iframe
+          src="https://ghbtns.com/github-btn.html?user=freevuehub&repo=freevuehub.github.io&type=star&count=true&size=large"
+          scrolling="0"
+          width="120px"
+          height="30px"
+        /> */}
+        <Menu
+          list={list}
+          className={props.className}
+          onBgClick={onMenuBgClick}
+        />
+        <SearchInput />
+      </div>
     </HeaderStyled>
   )
 }
