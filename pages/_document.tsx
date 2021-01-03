d9737cca70a3bcf85c3e976cb9158fa9014908d3
doo9392 <hackish@hanmail.net>
2021-01-03 21:23:43 +0900

    [refactor]: lint check

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,11 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-
-import { GA_TRACKING_ID } from "../lib/gtag";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
@@ -20,13 +18,13 @@ export default class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-          `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `
             }}
           />
         </Head>
