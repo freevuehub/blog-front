90e225345351960876ae60dd9b4033d29e1528e7
doo9392 <hackish@hanmail.net>
2021-01-13 22:07:51 +0900

    [improved]: pwa지원을 위한 head 수정

--- components/HeadSet.tsx
+++ components/HeadSet.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { colorSet } from '../lib'
 // import { useRouter } from 'next/router'
 
 interface IProps {
@@ -11,37 +12,63 @@ interface IProps {
 const HeadSet: React.FC<IProps> = (props) => {
   // const router = useRouter()
   const defaultImage = 'https://file.freevue.dev/images/8bf395a7-976c-442a-b87a-9346ce90638e.png'
+  const image = props.image || defaultImage
   const title = props.title
     ? `Freevue Blog | ${props.title}`
     : 'Freevue Blog'
   const keywords = props.keywords
     ? `freevue, front-end, 프리뷰, ${props.keywords}`
     : 'freevue, front-end, 프리뷰'
+  const description = props.description || 'Front-end Developer. FreeVue입니다.'
 
   return (
     <Head>
+      <meta charSet="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="theme-color" content={colorSet.primary} />
       <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
 
       <title>{title}</title>
 
+      <meta name="application-name" content="FreeVue Blog" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="FreeVue Blog" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-TileColor" content={colorSet.primary} />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="msapplication-TileImage" content="/icons/ms-icon-144x144.png" />
+
+      <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192"  href="/icons/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+
       <meta property="og:title" content={title} />
-      <meta name="twitter:title" content={title} />
-      {
-        props.description && (
-          <>
-            <meta name="description" content={props.description} />
-            <meta property="og:description" content={props.description} />
-            <meta name="twitter:description" content={props.description} />
-          </>
-        )
-      }
+      <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="FreeVue Blog" />
-      <meta property="og:image" content={props.image || defaultImage} />
-      <meta name="twitter:image" content={props.image || defaultImage} />
+      <meta property="og:image" content={image} />
       <meta property="og:url" content="https://freevue.dev" />
-      <meta name="twitter:url" content="https://freevue.dev" />
       <meta property="og:type" content="article" />
+
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      <meta name="twitter:url" content="https://freevue.dev" />
       <meta name="twitter:card" content="summary_large_image" />
 
       <link rel="image_src" href={defaultImage} />
