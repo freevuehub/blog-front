0374ff0bcf3ebc03598ab01021dea790f776a13d
doo9392 <hackish@hanmail.net>
2021-01-13 22:06:39 +0900

    [new]: next-pwa 적용

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  },
   env: {
     API_PROTOCOL: process.env.API_PROTOCOL,
     API_URL: process.env.API_URL,
     GOOGLE_ANALYTICE_ID: process.env.GOOGLE_ANALYTICE_ID,
   },
-}
+})
