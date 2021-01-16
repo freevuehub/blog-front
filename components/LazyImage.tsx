2c71d969d14cbd12e16c1f563f6561d2579a5284
freevue <hackish113@gmail.com>
2021-01-16 20:18:15 +0900

    [improved]: 이미지 확인

--- components/LazyImage.tsx
+++ components/LazyImage.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
-const LazyImage: React.FC<any> = (props) => (
-  <LazyLoadImage
-    delayTime={1000}
-    effect="blur"
-    {...props}
-  />
-)
+const LazyImage: React.FC<any> = (props) => {
+  useEffect(() => {
+    console.log(props.src)
+  }, [props])
+
+  return (
+    <LazyLoadImage
+      delayTime={1000}
+      effect="blur"
+      {...props}
+    />
+  )
+}
 
 export default LazyImage
