299677ddc61cf43cc82bb6212f2c1e032b80221a
freevue <hackish113@gmail.com>
2021-01-23 17:01:32 +0900

    [new]: login 페이지 추가

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,37 @@
-const LogginPage = () => {
+import styled from '@emotion/styled'
+import { breakPoint } from '../lib'
+import { ITheme } from '../types'
+
+const PageWrapStyled = styled.article`
+  display: flex;
+  width: 100%;
+  min-height: calc(100vh - 180px);
+  align-items: center;
+  justify-content: center;
+  
+  @media (max-width: ${breakPoint.mobile}) {
+    min-height: calc(100vh - 220px);
+  }
+`
+const LoginCardStyled = styled.div`
+  padding: 20px;
+  width: 100%;
+  max-width: 600px;
+  ${(props: ITheme) => ({
+    boxShadow: props.theme?.shadow.new,
+    color: props.theme?.text,
+  })}
+`
+
+const LoginPage = () => {
   return (
-    <div>Login</div>
+    <PageWrapStyled>
+      <LoginCardStyled>
+        <h1>Login</h1>
+        <hr />
+      </LoginCardStyled>
+    </PageWrapStyled>
   )
 }
 
-export default  LogginPage
+export default  LoginPage
