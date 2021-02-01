601483342585c996ce3d39856aaf3847b9a8bb6c
freevue <hackish113@gmail.com>
2021-02-01 20:27:29 +0900

    [improved]: 게시글에 미리보기 이미지 추가

--- components/PostListItem.tsx
+++ components/PostListItem.tsx
@@ -3,31 +3,31 @@ import styled from '@emotion/styled'
 import dayjs from 'dayjs'
 import { ITheme, IPostListItem } from '../types'
 import { breakPoint } from '../lib'
-// import { LazyImage } from './'
+import { LazyImage } from './'
 
-// const ImageWrapStyled = styled.div`
-//   width: 220px;
-//   height: 124px;
-//   position: relative;
-//   border-radius: 15px;
-//   box-shadow: ${(props: ITheme) => props.theme?.shadow.new};
-//   margin-right: 25px;
-//   overflow: hidden;
-//   .image {
-//     object-fit: cover;
-//   }
+const ImageWrapStyled = styled.div`
+  width: 220px;
+  height: 124px;
+  position: relative;
+  border-radius: 15px;
+  box-shadow: ${(props: ITheme) => props.theme?.shadow.material};
+  margin-right: 25px;
+  overflow: hidden;
+  .image {
+    object-fit: cover;
+  }
 
-//   @media (max-width: 840px) {
-//     width: 160px;
-//     height: 90px;
-//   }
-//   @media (max-width: 600px) {
-//     border-radius: 10px;
-//     width: 120px;
-//     height: 67.5px;
-//     margin-right: 15px;
-//   }
-// `
+  @media (max-width: 840px) {
+    width: 160px;
+    height: 90px;
+  }
+  @media (max-width: 600px) {
+    border-radius: 10px;
+    width: 120px;
+    height: 67.5px;
+    margin-right: 15px;
+  }
+`
 const TextWrapStyled = styled.div`
   position: relative;
   flex: 1;
@@ -90,17 +90,11 @@ const PostListItem: React.FC<IProps> = (props) => {
   return (
     <>
       {
-        // props.item.image && (
-        //   <ImageWrapStyled className="img-wrap">
-        //     <LazyImage
-        //       className="image"
-        //       width="100%"
-        //       height={124}
-        //       src={props.item.image}
-        //       alt={props.item.title}
-        //     />
-        //   </ImageWrapStyled>
-        // )
+        (props.item.image && !props.mini) && (
+          <ImageWrapStyled className="img-wrap">
+            <LazyImage className="image" src={props.item.image} />
+          </ImageWrapStyled>
+        )
       }
       <TextWrapStyled className={`text-wrap ${props.mini ? 'mini' : ''}`}>
         <span className="text-category">{props.item.category}</span>
