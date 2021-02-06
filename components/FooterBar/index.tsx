b8433e8be97d15b600a23f9aac03010ca1ab51cc
freevue <hackish113@gmail.com>
2021-02-06 11:53:31 +0900

    [improved]: 개인정보취급방침 링크 추가

--- components/FooterBar/index.tsx
+++ components/FooterBar/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import styled from '@emotion/styled'
+// import Link from 'next/link'
 import { breakPoint } from '../../lib'
 import SocialArea  from './SocialArea'
+import {ITheme} from "../../types";
 
 const FooterStyled = styled.footer`
   background: #000;
@@ -15,6 +17,16 @@ const FooterStyled = styled.footer`
     min-height: 120px;
     max-width: ${breakPoint.tabletPro};
     margin: 0 auto;
+    .right-side {
+      a {
+        font-size: 12px;
+        opacity: .5;
+        margin-right: 10px;
+        ${(props: ITheme) => ({
+          color: props.theme?.text
+        })}
+      }
+    }
   }
 
   @media (max-width: ${breakPoint.mobile}) {
@@ -29,7 +41,10 @@ const FooterBar: React.FC = () => {
     <FooterStyled>
       <div className="inner">
         <SocialArea />
-        <div>© <b>FreeVue</b></div>
+        <div className="right-side">
+          {/*<Link href="/privacy">개인정보처리방침</Link>*/}
+          © <b>FreeVue</b>
+        </div>
       </div>
     </FooterStyled>
   )
