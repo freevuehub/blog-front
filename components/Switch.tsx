1ed8a3018b2c9e9eccf139238dd96d513bfda384
freevue <hackish113@gmail.com>
2021-02-10 17:51:07 +0900

    [new]: switch component 추가

---
+++ components/Switch.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const SwitchWrapStyled = styled.div``
+
+const Switch: React.FC = () => {
+  return (
+    <SwitchWrapStyled>
+      <input type="checkbox"/>
+    </SwitchWrapStyled>
+  )
+}
+
+export default Switch
