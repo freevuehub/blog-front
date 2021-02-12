a2caaf444a1b820e9952f33f81448f4c7fb157d2
freevue <hackish113@gmail.com>
2021-02-12 21:33:41 +0900

    [refactor]: 소스 정리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,31 @@
 import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
+import { NextRouter, useRouter } from 'next/router'
 import { ApolloProvider } from '@apollo/client'
 import { Global, ThemeProvider } from '@emotion/react'
 import { client, colorSet } from '../lib'
 import { FontStyle, ResetStyle, MarkdownStyle } from '../styles'
-import { Layout } from '../components'
+import { Layout, SnackBar } from '../components'
 import * as gtag from '../lib/gtag'
 
 const mode: 'dark' | 'light' = 'dark'
 const theme = colorSet[mode]
+const onEffect = (router: NextRouter) => () => {
+  const onRouteChange = (url: URL) => {
+    gtag.pageview(url)
+  }
+  const effectCallBack = () => {
+    router.events.off('routeChangeComplete', onRouteChange)
+  }
+
+  router.events.on('rwouteChangeComplete', onRouteChange)
+
+  return effectCallBack
+}
 const App: React.FC<AppProps> = (props) => {
   const router = useRouter()
 
-  useEffect(() => {
-    const onRouteChange = (url: URL) => {
-      gtag.pageview(url)
-    }
-
-    router.events.on('routeChangeComplete', onRouteChange)
-
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChange)
-    }
-  }, [router.events])
+  useEffect(onEffect(router), [router.events])
 
   return (
     <ApolloProvider client={client}>
@@ -34,6 +36,7 @@ const App: React.FC<AppProps> = (props) => {
         <Layout>
           <props.Component {...props.pageProps} />
         </Layout>
+        <SnackBar />
       </ThemeProvider>
     </ApolloProvider>
   )
