71a1d6cb171d80b39be7b97da9a3947c8b6a7e0f
freevue <hackish113@gmail.com>
2021-02-24 16:47:06 +0900

    [improved]: any 타입 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,14 @@ import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { NextRouter, useRouter } from 'next/router'
 import { ApolloProvider } from '@apollo/client'
-import { Global, ThemeProvider } from '@emotion/react'
+import { Global, ThemeProvider, Theme } from '@emotion/react'
 import { client, colorSet } from '~/lib'
 import { FontStyle, ResetStyle, MarkdownStyle } from '~/styles'
 import { Layout, SnackBar } from '~/components'
 import * as gtag from '~/lib/gtag'
 
 const mode: 'dark' | 'light' = 'dark'
-const theme = colorSet[mode]
+const themeSet: Theme = colorSet[mode]
 const onEffect = (router: NextRouter) => () => {
   const onRouteChange = (url: URL) => {
     gtag.pageview(url)
@@ -29,10 +29,10 @@ const App: React.FC<AppProps> = (props) => {
 
   return (
     <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={themeSet}>
         <Global styles={FontStyle} />
         <Global styles={ResetStyle} />
-        <Global styles={(theme: any) => MarkdownStyle(theme)} />
+        <Global styles={(theme: Theme) => MarkdownStyle(theme)} />
         <Layout>
           <props.Component {...props.pageProps} />
         </Layout>
