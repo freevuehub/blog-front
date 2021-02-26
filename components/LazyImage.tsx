aca011db1be6e4759904b754adf75a6d46087043
freevue <hackish113@gmail.com>
2021-02-26 23:13:54 +0900

    [refactor]: alias 적용

--- components/LazyImage.tsx
+++ components/LazyImage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import styled from '@emotion/styled'
-import { ITheme } from '../types'
+import { ITheme } from '~/types'
 
 interface  IProps {
   src: string
