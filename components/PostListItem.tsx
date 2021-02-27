89aa43cb3ee96267079982f84273ff5205481a93
freevue <hackish113@gmail.com>
2021-02-27 14:30:30 +0900

    [refactor]: alias 적용

--- components/PostListItem.tsx
+++ components/PostListItem.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from '@emotion/styled'
 import dayjs from 'dayjs'
 import { ITheme, IPostListItem } from '../types'
-import { breakPoint } from '../lib'
+import { breakPoint } from '~/lib'
 import { LazyImage } from './'
 
 const ImageWrapStyled = styled.div`
