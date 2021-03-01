5c95f27e1f5b5c2064e26172f0fc89767c8f51de
freevue <hackish113@gmail.com>
2021-03-01 14:04:47 +0900

    [refactor]: alias

--- components/GithubContributions.tsx
+++ components/GithubContributions.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import { ITheme, IContribution } from '../types'
-import { breakPoint } from '../lib'
+import { ITheme, IContribution } from '~/types'
+import { breakPoint } from '~/lib'
 
 interface IProps {
   data: IContribution[][]
