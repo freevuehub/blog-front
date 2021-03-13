da94760ad80296e1de57b00e1e6ba0668092f486
freevue <hackish113@gmail.com>
2021-03-13 21:56:58 +0900

    [improved]: scroll hooks 기능 향상

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -2,9 +2,13 @@ import { useState, useEffect } from 'react'
 
 export const useScroll = () => {
   const [scrollTop, setScrollTop] = useState<number>(0)
+  const [isUp, setIsUp] = useState<boolean>(false)
+  const [isDown, setIsDown] = useState<boolean>(false)
 
   useEffect(() => {
     window.onscroll = function() {
+      setIsUp(scrollTop > this.scrollY)
+      setIsDown(scrollTop <= this.scrollY)
       setScrollTop(this.scrollY)
     }
 
@@ -13,5 +17,9 @@ export const useScroll = () => {
     }
   }, [])
 
-  return scrollTop
+  return {
+    scrollTop,
+    isUp,
+    isDown,
+  }
 }
